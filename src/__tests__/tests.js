0ee7774d63c117012041bb041988cecb02003140
SilantevR <craft_89@inbox.ru>
2022-09-22 15:46:30 +0300

    task 2

---
+++ src/__tests__/tests.js
@@ -0,0 +1,16 @@
+import sort from "../index";
+
+test("array sort", () => {
+  const arr = [
+    { name: "мечник", health: 10 },
+    { name: "маг", health: 100 },
+    { name: "лучник", health: 80 },
+  ];
+
+  const result = [
+    { name: "маг", health: 100 },
+    { name: "лучник", health: 80 },
+    { name: "мечник", health: 10 },
+  ];
+  expect(sort(arr)).toEqual(result);
+});
